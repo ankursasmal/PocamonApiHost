3c929fb7dcffc7f80958c719b12a44434c210322
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-06-21 17:35:06 +0530

    api host

--- db/cnnection.js
+++ db/cnnection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 let mongoose=require('mongoose');
- mongoose.connect(process.env.MONGO_URL).then(()=>{
+ mongoose.connect(`${process.env.MONGO_URL}`).then(()=>{
     console.log('db connect')
 })
 .catch((e)=>{
